c73eb99b0a50943f8ce22e00ca61002d2d0fdbfa
mohammadeltawil0 <147886761+mohammadeltawil0@users.noreply.github.com>
2025-06-16 20:05:59 -0400

    Create DemoThree.java

---
+++ java/fundamentals/DemoThree.java
@@ -0,0 +1,17 @@
+public class DemoThree {
+    
+    public static void main(String[] args)
+    {
+        //int x = 2, y = 2;
+        //System.out.println("Addition of two numbers = " + x+y);
+        //System.out.println("Addition of two numbers = " + (x+y));
+        //System.out.println("Result = " + (x - y));
+        //System.out.println("Multiplication = " + x * y);
+
+        int a = 1, b = 1;
+        System.out.println("" + (a + b));
+
+    }
+}
+//2+2 = 4
+//2*2 = 4
