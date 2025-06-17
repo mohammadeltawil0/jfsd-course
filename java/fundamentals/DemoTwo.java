9e6255b9e487cf91686918f5be70daae59a9c696
mohammadeltawil0 <147886761+mohammadeltawil0@users.noreply.github.com>
2025-06-16 20:05:28 -0400

    Create DemoTwo.java

---
+++ java/fundamentals/DemoTwo.java
@@ -0,0 +1,22 @@
+public class DemoTwo {
+    
+    public static void main(String[] args)
+    {
+        int x;
+        int y;
+
+        //int x, y;
+
+        x = 19;
+        y = 20;
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+
+        x = 126;
+        y = 128;
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+
+    }
+}
+
