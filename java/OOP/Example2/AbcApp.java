c8d71e7216be57c545d25f78f349a00fe1de2c50
mohammadeltawil0 <147886761+mohammadeltawil0@users.noreply.github.com>
2025-06-16 21:28:19 -0400

    Create AbcApp.java

---
+++ java/OOP/Example2/AbcApp.java
@@ -0,0 +1,15 @@
+public class AbcApp {
+    public static void main(String[] args) {
+        Abc obj = new Abc(101);
+        obj.showXnY();
+
+        Abc obj2 = new Abc(11111);
+        obj2.showXnY();
+
+        Abc obj3 = new Abc();
+        obj3.showXnY();
+
+        Abc obj4 = new Abc(1,2);
+        obj4.showXnY();
+    }
+}
