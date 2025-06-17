da31e6a003f3bd88afd9032749147acfc324a484
mohammadeltawil0 <147886761+mohammadeltawil0@users.noreply.github.com>
2025-06-16 20:09:43 -0400

    Create ExerciseTwo.java

---
+++ java/fundamentals/ExerciseTwo.java
@@ -0,0 +1,39 @@
+public class ExerciseTwo {
+    
+    public static void main(String[] args)
+    {
+        // int num1 = 120;
+        // int num2 = 350;
+        // int num3 = 245;
+        // int num4 = 120;
+        // //                                              true    &&  true                                            
+        // System.out.println("num2>num1 && num3<num2 :: " + ((num2 > num1) && (num3 < num2)));
+        // //                                                  true   && true           && true
+        // System.out.println("num1<=num4 && num3>num1 && num2>num4 :: " + (num1<=num4 && num3>num1 && num2>num4));
+        // //                                                                 true     &&   true    && false
+        // System.out.println("num1<=num4 && num3>num1 && num2<num4 :: " + (num1<=num4 && num3>num1 && num2<num4));
+        // //                                                                 true     ||   true    || false
+        // System.out.println("num1<=num4 || num3>num1 || num2<num4 :: " + (num1<=num4 || num3>num1 || num2<num4));
+        // //                                                                 true     &&   true    && true
+        // System.out.println("num1<=num4 && num3>num1 && !(num2<num4) :: " + (num1<=num4 && num3>num1 && !(num2<num4)));
+
+
+
+
+
+
+        // int x = 5;
+        // int y = x++;
+        // x++;
+        // System.out.println("x = " + x);
+        // System.out.println("y = " + y);
+
+        int x = 5;
+        int y = ++x;
+        //++x;
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+    }
+}
+
+
