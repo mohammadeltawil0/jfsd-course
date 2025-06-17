0975e23b9ce27dcb9bd048c08ced1745ed035c02
mohammadeltawil0 <147886761+mohammadeltawil0@users.noreply.github.com>
2025-06-16 20:11:36 -0400

    Create MinimumNumber.java

---
+++ java/fundamentals/MinimumNumber.java
@@ -0,0 +1,11 @@
+public class MinimumNumber {
+    public static void main(String[] args) {
+        int x = 1234, y = 348; 
+
+        if(x<y)
+            System.out.println("x is minimum");
+        
+        if(y<x)
+            System.out.println("y is minimum");
+    }
+}
