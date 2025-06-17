84122cdb1c9ccf1f670bab24201e04d31d83014a
mohammadeltawil0 <147886761+mohammadeltawil0@users.noreply.github.com>
2025-06-16 20:17:49 -0400

    Create ForDemoThree.java

---
+++ java/fundamentals/ForDemoThree.java
@@ -0,0 +1,29 @@
+public class ForDemoThree {
+    public static void main(String[] args) {
+        // for (int i = 1; i<=10; i++) 
+        //     System.out.print(i*2 + ", ");
+
+        // for (int i = 2; i <=20; i += 2) {
+        //     System.out.print(i + ",");
+        // }
+        
+        // for (int i = 2; i <= 20; i += 2) {
+        //     System.out.print(i + " ");
+        // }
+
+    //    int counter = 0;
+    //    for (int i = 1; i <= 10; i++) {
+    //        System.out.print(i + counter + ", ");
+    //        counter+=i;
+    //    }
+
+        int sum = 0;        
+        for (int i = 1; i <= 9; i++) 
+        {           
+            sum += i;            
+            System.out.print(sum + ",");
+        }
+    }
+}
+
+
