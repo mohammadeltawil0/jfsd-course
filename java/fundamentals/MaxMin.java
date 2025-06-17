ead4c533570fdf006a9d50d567c44d6e71117818
mohammadeltawil0 <147886761+mohammadeltawil0@users.noreply.github.com>
2025-06-16 20:12:33 -0400

    Create MaxMin.java

---
+++ java/fundamentals/MaxMin.java
@@ -0,0 +1,17 @@
+public class MaxMin {
+    public static void main(String[] args) {
+        //write a program to find maximum and minimum between two numbers
+        int x = 5, y = 6;
+
+        if (x>y){
+            System.out.println("x is maximum");
+            System.out.println("y is minimum");
+        }else {
+            System.out.println("y is maximum");
+            System.out.println("x is minimum");
+        }
+    }
+}
+
+
+
