4025d7070467fac7846533ae96d6f85f3ba1fb9d
mohammadeltawil0 <147886761+mohammadeltawil0@users.noreply.github.com>
2025-06-16 20:10:23 -0400

    Create IfDemoOne.java

---
+++ java/fundamentals/IfDemoOne.java
@@ -0,0 +1,10 @@
+public class IfDemoOne {
+    public static void main(String[] args) {
+        int x = -11;
+
+        if (x>0){
+            System.out.println("x is a positive number");
+            System.out.println("Done");
+        }        
+    }    
+}
