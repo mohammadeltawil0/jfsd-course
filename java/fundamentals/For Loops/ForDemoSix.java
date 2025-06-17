05fd02fd6eccbe041bd2216031503b5e03b100f6
mohammadeltawil0 <147886761+mohammadeltawil0@users.noreply.github.com>
2025-06-16 20:50:58 -0400

    Create ForDemoSix.java

---
+++ java/fundamentals/For Loops/ForDemoSix.java
@@ -0,0 +1,17 @@
+public class ForDemoSix {
+    public static void main(String[] args) {
+        int i=1;
+        for(;;)
+        {
+            // if(i>10)
+            //     break;
+            // System.out.println(i++);            
+            // //i++;
+
+            System.out.println(i++);
+            if(i>10)
+                break;
+        }
+
+    }
+}
