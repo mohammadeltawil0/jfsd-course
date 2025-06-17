600983ab59535ed19ab54702a41498fa03185bc2
mohammadeltawil0 <147886761+mohammadeltawil0@users.noreply.github.com>
2025-06-16 20:16:24 -0400

    Create IfDemoTwo.java

---
+++ java/fundamentals/IfDemoTwo.java
@@ -0,0 +1,19 @@
+public class IfDemoTwo {
+    public static void main(String[] args) {
+        int x = 0;
+
+        if (x>0)
+            System.out.println("x is a positive number");
+        
+        if(x<0)
+            System.out.println("x is a negative number");
+
+        if(x==0)
+            System.out.println("x is zero");
+    }
+}
+
+
+
+//write a program to find minimum between two numbers, let x and y
+
