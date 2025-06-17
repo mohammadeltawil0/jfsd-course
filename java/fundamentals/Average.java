eb6f040b8211ca1567e4fca35fae9c0662f0e37a
mohammadeltawil0 <147886761+mohammadeltawil0@users.noreply.github.com>
2025-06-16 20:06:31 -0400

    Create Average.java

---
+++ java/fundamentals/Average.java
@@ -0,0 +1,16 @@
+public class Average {
+    public static void main(String[] args) {
+    
+        float numOne = 6.53f;
+        float numTwo = 7.98f;
+        float numThree = 2.89f;
+        float average = (numOne+numTwo+numThree)/3.0f;
+        //String officeAddress= "";
+        System.out.println("Average:" + average);
+        
+
+        }
+}
+
+//calculate the simple interest of any principal amount
+
