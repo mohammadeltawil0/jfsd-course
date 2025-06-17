a54e18ea0780a87e8073c5a07edc298416b8ed7b
mohammadeltawil0 <147886761+mohammadeltawil0@users.noreply.github.com>
2025-06-16 20:18:49 -0400

    Create ForDemoFive.java

---
+++ java/fundamentals/ForDemoFive.java
@@ -0,0 +1,16 @@
+public class ForDemoFive {
+    public static void main(String[] args) 
+    {
+        int i;
+        for(i=1;i<=5;i++)
+        {
+            //int sum=0;
+            System.out.println(i);
+            //sum = sum + i;
+        }
+        System.out.println(i);
+        //System.out.println(sum);
+    }
+}
+
+
