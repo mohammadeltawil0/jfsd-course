f80f67684ab07336586494deefb2f369e9ef8842
mohammadeltawil0 <147886761+mohammadeltawil0@users.noreply.github.com>
2025-06-16 20:11:02 -0400

    Create MaximumNumber.java

---
+++ java/fundamentals/MaximumNumber.java
@@ -0,0 +1,17 @@
+public class MaximumNumber {
+    public static void main(String[] args) {
+        //write a program to find maximum between two numbers, let x and y
+        int x = 11167, y = 4839;
+
+        //x = 1123;
+        //y = 1234;
+
+        if (x > y)       
+            System.out.println("x is maximum");
+        
+        if( y > x)
+            System.out.println("y is maximum");
+    }
+}
+
+
