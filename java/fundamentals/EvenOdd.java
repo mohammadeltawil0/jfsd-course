420404f91a04de853d4ae69f6ce42908aaf7522b
mohammadeltawil0 <147886761+mohammadeltawil0@users.noreply.github.com>
2025-06-16 20:12:04 -0400

    Create EvenOdd.java

---
+++ java/fundamentals/EvenOdd.java
@@ -0,0 +1,13 @@
+public class EvenOdd {
+    public static void main(String[] args) {
+        //Write a progrm to check whether a given number is even or odd
+      int num = 2121;
+ 
+        if (num%2 == 0) 
+            System.out.println(num + " is Even.");
+        else 
+            System.out.println(num + " is Odd.");        
+    }
+}
+
+
