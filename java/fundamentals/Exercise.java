43a53d89039f5fe03c6e5c45ad0f0f3f6b433da2
mohammadeltawil0 <147886761+mohammadeltawil0@users.noreply.github.com>
2025-06-16 20:08:33 -0400

    Create Exercise.java
    Includes conceptual questions on Java

---
+++ java/fundamentals/Exercise.java
@@ -0,0 +1,94 @@
+public class Exercise {
+
+    public static void main(String[] args)
+    {
+    // int x , y ;    
+    // // Swap x and y using third variable        
+    // x = 5;
+    // y = 7;
+
+    // // int temp = x ;
+    // // x = y;
+    // // y = temp;
+
+    
+    // // Swap x and y without using third variable
+    // x = x+y;
+    // y = x-y;
+    // x = x-y;
+    // System.out.println("x = " + x);
+    // System.out.println("y = " + y);
+
+
+    int x = 5;
+    int y = 10;
+    int z = 4;
+
+    System.out.println(x>5);
+    System.out.println(z <= x); //less than or equal to
+    boolean isMarried = false;
+
+
+
+    }
+}
+
+
+//Q. where does the program execute?  - CPU (Processor)
+//Q. where does the data store during program execution? - RAM
+//Q. Where OS is being stored? - Secondary Storage
+//Q. Where does OS execute? - CPU
+//Q. Who is responsible to execute the code?    - Processor
+//Q. who submit your program to the processor to execute? - OS
+//Q. What is program and what is process?
+//Program - code - Passive Entity
+//Process - program under execution  - Active Entity 
+
+//Life cycle of process 
+//What is the other memory name and location that store data during program execution?
+
+
+
+
+
+
+
+
+
+// Source Code - Java File - .java
+//     |
+//     | compile
+//     |
+//     V
+// Byte Code - class file - .class
+//     |
+//     | interpret and run
+//     |
+//     V
+// Executable code / run
+
+
+/*
+ * Syntax 
+ *   declare variables, print info/data on the screen
+ *   basic logic building
+ */
+
+//  sum x+y
+//  average  (x+y)/2
+//  swap     
+
+
+//Operators 
+/*
+    Arithmetic 
+    Relational (boolean value = true/false)== ,  != , >, < , >= (greater than or equal to ), <=(less than or equal to)
+    Logical  && (and), || (or) , ! (NOT)
+    Assignment
+    Unary
+    Ternary
+    Bitwise
+    Shift
+ */
+
+
