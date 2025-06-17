165d5952c06a13186dca65bdc2e2257f3d182cd5
mohammadeltawil0 <147886761+mohammadeltawil0@users.noreply.github.com>
2025-06-16 21:11:18 -0400

    Create Array1.java

---
+++ java/fundamentals/Arrays/Array1.java
@@ -0,0 +1,18 @@
+public class ArrayOne {
+    public static void main(String[] args) {
+        int arr[];
+        arr = new int[5];
+
+        arr[0] = 111;
+        arr[1] = 222;
+        arr[2] = 333;
+        arr[3] = 444;
+        arr[4] = 555;
+
+        System.out.println(arr[0]);
+        System.out.println(arr[1]);
+        System.out.println(arr[2]);
+        System.out.println(arr[3]);
+        System.out.println(arr[4]);
+    }
+}
