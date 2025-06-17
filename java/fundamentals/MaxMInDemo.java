73242ef8f6bc7f6e674d9414c0f73ab175b49584
mohammadeltawil0 <147886761+mohammadeltawil0@users.noreply.github.com>
2025-06-16 20:12:58 -0400

    Create MaxMInDemo.java

---
+++ java/fundamentals/MaxMInDemo.java
@@ -0,0 +1,26 @@
+public class MaxMinDemo {
+    public static void main(String[] args) {
+        //Write a program to check whether a given number is lesser than other number,
+        //or greater than other number or equal to number
+
+        int x = 1236;
+        int y = 1236;
+
+        if(x>y)
+            System.out.println("x is greater than y");
+        else if(x<y)
+            System.out.println("x is lesser than y");
+        else 
+            System.out.println("x is equal to y");
+
+
+        int a = 0;
+        if(a>0)
+            System.out.println("a is a positive number");
+        else if(a<0)
+            System.out.println("a is a negative number");    
+        else
+            System.out.println("a is zero");
+    }
+}
+
