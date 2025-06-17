5cf5bf3bb012ff65a603dc86cfb0f61b4a635e26
mohammadeltawil0 <147886761+mohammadeltawil0@users.noreply.github.com>
2025-06-16 20:15:52 -0400

    Create DayName.java

---
+++ java/fundamentals/DayName.java
@@ -0,0 +1,35 @@
+public class DayName {
+    
+    public static void main(String[] args) {
+        int day = 5;
+
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;    
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;            
+            default:
+                System.out.println("Invalid Value");                
+        }
+        System.out.println("Done");    
+    }    
+}
+//write a program to check whether a given character is consonant or vowel
+
